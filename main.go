228cb0b9627c69cbdba6243c42fd238557d8bf05
DESKTOP-VEVQNJG\sutti <srangseethammakul@gmail.com>
2021-11-18 22:25:45 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"githuc.com/srangseethammakul/gobasic/variables"
+)
+
+func main() {
+	// fmt.Println("Hello World!")
+	// fmt.Println(quote.HelloV3())
+	variables.Learn()
+}
