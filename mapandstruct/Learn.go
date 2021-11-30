7e363dca34d310e7994d3fd77822ffe1211aad4d
DESKTOP-VEVQNJG\sutti <srangseethammakul@gmail.com>
2021-11-30 22:30:05 +0700

    struct

--- mapandstruct/Learn.go
+++ mapandstruct/Learn.go
@@ -4,6 +4,12 @@ import "fmt"
 
 func Learn() {
 	//maps (key, value)
+
+	var m2 = make(map[string]int)
+	m2["a"] = 10
+	m2["b"] = 20
+	fmt.Println(m2["b"])
+
 	m := map[string]int{"token": 10, "access": 20}
 	fmt.Println(m)
 	fmt.Println(m["access"])
@@ -26,4 +32,26 @@ func Learn() {
 	for key, value := range m {
 		fmt.Printf("golang %v %v \n", key, value)
 	}
+
+	// Struct
+	type User struct {
+		id       int
+		username string
+		password string
+	}
+
+	john := User{
+		id:       1,
+		username: "Johny Dep",
+		password: "123123",
+	}
+	fmt.Println(john.username)
+	john.password = "1234"
+	fmt.Println(john.password)
+
+	users := []User{
+		{id: 2, username: "Act", password: "123"},
+		{id: 3, username: "Act2", password: "1234"},
+	}
+	fmt.Println(users[0].password)
 }
